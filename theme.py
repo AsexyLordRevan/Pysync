761848d210a67064ef1e7ecb0644515a384a712f
AsexyLordRevan <155589214+AsexyLordRevan@users.noreply.github.com>
2024-12-26 01:46:49 -0800

    Update theme.py

--- theme.py
+++ theme.py
@@ -2,8 +2,8 @@ software = [
 {"name":"waybar", "location":"~/.config/waybar", "format":"coloursgtk.css"},
 {"name":"hyprland", "location":"~/.config/hypr", "format":"colours.conf"},
 {"name":"rofi", "location":"~/.config/rofi", "format":"colours.rasi"},
-{"name":"betterdiscord", "location":"/home/revan/.config/Vencord/themes/rose-pine.theme.css", "format":"search&replace"},
-{"name":"kitty", "location":"/home/revan/.config/kitty/kitty.conf", "format":"search&replace"},
+{"name":"betterdiscord", "location":"/home/USER/.config/Vencord/themes/rose-pine.theme.css", "format":"search&replace"},
+{"name":"kitty", "location":"/home/USER/.config/kitty/kitty.conf", "format":"search&replace"},
 ]
 
 commands = [
